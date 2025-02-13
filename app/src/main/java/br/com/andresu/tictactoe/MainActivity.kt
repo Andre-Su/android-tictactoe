54b6fe2ddab3e97730d581f6b18a63fa74e15051
Andre.jar <82320381+Andre-Su@users.noreply.github.com>
2025-02-13 17:05:51 -0300

    Atualizar página principal

--- app/src/main/java/br/com/andresu/tictactoe/MainActivity.kt
+++ app/src/main/java/br/com/andresu/tictactoe/MainActivity.kt
@@ -1,20 +1,36 @@
 package br.com.andresu.tictactoe
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import br.com.andresu.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        // Configurar View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // Configurar View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Adicionar Fragmentos
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_grid, GameGridFragment())
+            replace(R.id.fragment_container_menu, GameMenuFragment())
         }
     }
 }

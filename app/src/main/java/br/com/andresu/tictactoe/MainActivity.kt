f93304340598fa636ca744f0a09a0353a024b7da
Andre.jar <82320381+Andre-Su@users.noreply.github.com>
2025-02-13 17:11:47 -0300

    Otimizar Imports

--- app/src/main/java/br/com/andresu/tictactoe/MainActivity.kt
+++ app/src/main/java/br/com/andresu/tictactoe/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import br.com.andresu.tictactoe.databinding.ActivityMainBinding
 
